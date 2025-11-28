b6b83a3c536224028e862fb6ae112e989886ec1d
Bryce Pinkerton <bpink354@gmail.com>
2025-11-28 13:17:44 -0500

    set  player to seperate scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED := 300.0
-const FRICTION := 0.8
+const SPEED := 30.0
+const FRICTION := 0.5
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 
@@ -16,9 +18,21 @@ func _physics_process(delta):
 		velocity.y += SPEED
 	
 	
+	
 	velocity.normalized()
 	
 
 	move_and_slide()
 	velocity.x *= FRICTION
 	velocity.y *= FRICTION
+
+	if(velocity.x < 0.1 and velocity.x > -0.1):
+		velocity.x = 0.0
+	if(velocity.y < 0.1 and velocity.y > -0.1):
+		velocity.y = 0.0
+
+
+	if(velocity.x != 0 or velocity.y != 0):
+		anim.play("walk")
+	else:
+		anim.play("default")
