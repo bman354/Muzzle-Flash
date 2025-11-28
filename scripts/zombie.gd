35481f5e53788cd25ed65726315b19bfd2a8857c
CharcoalNuggets <dgin@csuchico.edu>
2025-11-28 14:55:29 -0800

    zombert leg work

--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -1,21 +1,28 @@
 extends CharacterBody2D
 
-
-const speed = 40
+const SPEED := 30.0
 
 @export var player: Node2D
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready():
+	pathfind()
 
 func _physics_process(_delta: float) -> void:
-	var dir = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = dir * speed
+	var dir =  to_local(nav_agent.target_position).normalized()	
+	velocity = dir * SPEED
 	move_and_slide()	
-
+	if (velocity.x != 0.0 and velocity.y != 0.0):
+		anim.play("walking")
+	#print(nav_agent.target_position)
 
 
 func pathfind() -> void:
-	nav_agent.target_position = player.global_position
-	
+	nav_agent.target_position = player.position
+	#print("pathfind called")
 
-func _on_timer_timeout():
+
+func _on_timer_timeout() -> void:
 	pathfind()
+	#print("timer finished")
