ebabab75d43f355cf5c02f55b1dceca71b881da5
CharcoalNuggets <dgin@csuchico.edu>
2025-11-28 12:49:05 -0800

    zombie vegetable

--- scripts/zombie.gd
+++ scripts/zombie.gd
@@ -1,11 +1,21 @@
-extends AnimatableBody2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const speed = 40
+
+@export var player: Node2D
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _physics_process(_delta: float) -> void:
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+func pathfind() -> void:
+	nav_agent.target_position = player.global_position
+	
+
+func _on_timer_timeout():
+	pathfind()
