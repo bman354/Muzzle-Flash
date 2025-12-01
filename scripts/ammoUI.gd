e7dafa002099c8ec1ae9f8fc8e6de528abf108b6
Bryce Pinkerton <bpink354@gmail.com>
2025-12-01 17:18:25 -0500

    main menu and some scaling

--- scripts/ammoUI.gd
+++ scripts/ammoUI.gd
@@ -6,10 +6,21 @@ extends Control
 const full_shell_texture = preload("res://assets/full_shell.png")
 const empty_shell_texture = preload("res://assets/empty_shell.png")
 
+@export var shell_scale = Vector2(1,1)
+
+
+
 func _ready():
 	_on_player_ammo_changed(2)
+	shell1.scale = shell_scale
+	shell2.scale = shell_scale
+	
 
 func _on_player_ammo_changed(amount: int):
+	
+	shell1.scale = shell_scale
+	shell2.scale = shell_scale
+	
 	match amount:
 		0:
 			shell1.texture = empty_shell_texture
